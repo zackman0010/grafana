70a2d856ec5b4f860d3d618759ff228d8eec7968
yaellec <yaelle.chaudy@grafana.com>
2024-12-06 17:18:41 +0100

    ran script and removed js transpiled files

--- packages/grafana-runtime/src/analytics/events/scripts/eventsTrackingInformation.ts
+++ packages/grafana-runtime/src/analytics/events/scripts/eventsTrackingInformation.ts
@@ -48,7 +48,7 @@ const eventList = [
         required: true,
       },
     },
-    stage: 'businessy',
+    stage: 'timeboxed',
     eventFunction: 'megaMenuOpened',
   },
   {
@@ -64,7 +64,7 @@ const eventList = [
         required: true,
       },
     },
-    stage: 'businessy',
+    stage: 'timeboxed',
     eventFunction: 'megaMenuDocked',
   },
   {
@@ -87,6 +87,37 @@ const eventList = [
     },
     stage: 'timeboxed',
     eventFunction: 'createReturnToPrevious',
+  },
+  {
+    repoName: 'grafana',
+    owner: 'Grafana Frontend Squad',
+    product: 'return_to_previous',
+    eventName: 'button_dismissed',
+    description: 'User dismissed a return to previous button',
+    properties: {
+      action: {
+        description: 'The action the user took to dismiss the button',
+        type: 'string',
+        required: true,
+      },
+      page: {
+        description: 'The page the user was on when the button was dismissed',
+        type: 'string',
+        required: true,
+      },
+    },
+    stage: 'timeboxed',
+    eventFunction: 'dismissReturnToPrevious',
+  },
+  {
+    repoName: 'grafana',
+    owner: 'Grafana Frontend Squad',
+    product: 'e2c',
+    eventName: 'delete_token_clicked',
+    description: 'User clicked on the delete token button',
+    properties: {},
+    stage: 'timeboxed',
+    eventFunction: 'trackDeleteTokenClicked',
   },
   {
     repoName: 'grafana',
