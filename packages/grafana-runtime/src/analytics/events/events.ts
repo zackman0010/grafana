826954775fe08f50b4fa1614b6b6bc744f604e72
yaellec <yaelle.chaudy@grafana.com>
2024-12-06 15:53:47 +0100

    removed events folder

--- packages/grafana-runtime/src/analytics/events/events.ts
+++
@@ -1,8 +0,0 @@
-import { reportInteraction } from '../utils';
-
-import { EventTrackingProps } from './types';
-
-export const reportTrackingEvent = (props: EventTrackingProps) => {
-  const repo = props.repo || 'grafana';
-  reportInteraction(`${repo}_${props.product}_${props.eventName}`, props.properties);
-};
