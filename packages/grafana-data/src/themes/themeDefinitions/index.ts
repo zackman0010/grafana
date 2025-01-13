6d86627a5932914dd75cb3d4cfd3c6c4b5543d9f
Ashley Harrison <ashley.harrison@grafana.com>
2025-01-13 17:18:51 +0000

    add some more themes

--- packages/grafana-data/src/themes/themeDefinitions/index.ts
+++ packages/grafana-data/src/themes/themeDefinitions/index.ts
@@ -1,1 +1,5 @@
+export { default as mars } from './mars.json';
 export { default as matrix } from './matrix.json';
+export { default as synthwave } from './synthwave.json';
+export { default as tron } from './tron.json';
+export { default as zen } from './zen.json';
