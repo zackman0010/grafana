d713c5f774bdb05ddb8949cfada6ac91ef89adc7
edpoole <ed@edpoole.me>
2025-01-14 18:16:59 +0000

    Add three new base g12 themes

--- packages/grafana-data/src/themes/themeDefinitions/index.ts
+++ packages/grafana-data/src/themes/themeDefinitions/index.ts
@@ -1,1 +1,4 @@
 export { default as matrix } from './matrix.json';
+export { default as g12 } from './g12.json';
+export { default as g12Green } from './g12Green.json';
+export { default as g12Pastel } from './g12Pastel.json';
