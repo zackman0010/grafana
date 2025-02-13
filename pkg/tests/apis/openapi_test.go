155d75d586393c38b45e01201af36dd21142935f
Mat Ryer <matryer@users.noreply.github.com>
2025-02-13 10:51:22 +0000

    add investigations group?

--- pkg/tests/apis/openapi_test.go
+++ pkg/tests/apis/openapi_test.go
@@ -73,6 +73,9 @@ func TestIntegrationOpenAPIs(t *testing.T) {
 	}, {
 		Group:   "provisioning.grafana.app",
 		Version: "v0alpha1",
+	}, {
+		Group:   "investigations.grafana.app",
+		Version: "v0alpha1",
 	}}
 	for _, gv := range groups {
 		VerifyOpenAPISnapshots(t, dir, gv, h)
