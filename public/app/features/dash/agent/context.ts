05ca3b37a78340ef734b1a6d4771d0182cdd86fb
Matias Chomicki <matyax@gmail.com>
2025-02-28 16:08:11 +0100

    Extend URLContext and add time range context

--- public/app/features/dash/agent/context.ts
+++ public/app/features/dash/agent/context.ts
@@ -1,13 +1,17 @@
-import { UrlQueryMap, urlUtil } from "@grafana/data";
-import { locationService } from "@grafana/runtime";
+import { UrlQueryMap, urlUtil } from '@grafana/data';
+import { locationService } from '@grafana/runtime';
 
 // @ts-expect-error
 window.getContext = () => {
-  console.log(getCurrentURLContext());
-}
+  const urlContext = getCurrentURLContext();
+  console.log(urlContext);
+  const timeRangeContext = getCurrentTimeRangeContext(urlContext);
+  console.log(timeRangeContext);
+};
 
 interface URLContext {
   url: string;
+  appName: string;
   title: string;
   params: UrlQueryMap;
 }
@@ -16,16 +20,28 @@ function getCurrentURLContext(): URLContext {
   const params = filterContextRelevantParams(urlUtil.getUrlSearchParams());
   return {
     url: location.pathname,
+    appName: locationToAppName(location.pathname),
     title: document.title,
     params,
+  };
+}
+
+function locationToAppName(pathname: string) {
+  if (pathname === '/') {
+    return 'Grafana home page';
+  } else if (pathname.includes('grafana-lokiexplore-app')) {
+    return 'Grafana Drilldown Logs, the queryless app to browse Loki Logs';
+  } else if (pathname.includes('/explore')) {
+    return 'Grafana Explore';
   }
+  return '';
 }
 
 function filterContextRelevantParams(params: UrlQueryMap) {
   const filteredParams: UrlQueryMap = {};
   for (const key in params) {
     if (Array.isArray(params[key])) {
-      const filteredArray = params[key].filter(element => element !== '') as string[] | number[] | boolean[];
+      const filteredArray = params[key].filter((element) => element !== '') as string[] | number[] | boolean[];
       if (filteredArray.length > 0) {
         filteredParams[key] = filteredArray;
       }
@@ -34,4 +50,14 @@ function filterContextRelevantParams(params: UrlQueryMap) {
     }
   }
   return filteredParams;
+}
+
+function getCurrentTimeRangeContext(urlContext?: URLContext) {
+  if (urlContext && urlContext.params.from && urlContext.params.to) {
+    if (urlContext.params.from.toString().includes('now-')) {
+      return urlContext.params.from;
+    }
+    return `${urlContext.params.from} to ${urlContext.params.to}`;
+  }
+  return document.querySelector('[data-testid~="TimePicker"]')?.getAttribute('aria-label')?.replace('Time range selected: ', '') ?? '';
 }
