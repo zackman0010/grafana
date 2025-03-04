d4bcdead76778a6cc1df71021aa0e0744672db2c
Matias Chomicki <matyax@gmail.com>
2025-03-04 17:14:19 +0000

    Dashboards: add autocomplete and navigation support (#25)

--- public/app/features/dash/agent/tools/index.ts
+++ public/app/features/dash/agent/tools/index.ts
@@ -3,6 +3,7 @@ import { getCurrentTimeTool } from './getCurrentTime';
 import { listDatasourcesTool } from './listDatasources';
 import { lokiLabelNamesTool } from './lokiLabelNames';
 import { lokiLabelValuesTool } from './lokiLabelValues';
+import { navigateToDashboardTool } from './navigateToDashboard';
 import { navigateToDrilldownLogs } from './navigateToDrilldownLogs';
 import { navigateToExploreTool } from './navigateToExplore';
 import { prometheusInstantQueryTool } from './prometheusInstantQuery';
@@ -22,6 +23,7 @@ export const tools = [
   navigateToExploreTool,
   navigateToDrilldownLogs,
   getCurrentTimeTool,
+  navigateToDashboardTool,
 ];
 
 export const toolsByName = tools.reduce(
