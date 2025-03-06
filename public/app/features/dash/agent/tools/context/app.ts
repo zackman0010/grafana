6a452b5d342b8d87b9b5ff076f9e87cd3dedf128
Mat Ryer <matryer@users.noreply.github.com>
2025-03-06 22:37:30 +0000

    more drilldown apps awareness

--- public/app/features/dash/agent/tools/context/app.ts
+++ public/app/features/dash/agent/tools/context/app.ts
@@ -2,9 +2,13 @@ import { PageContext } from './page';
 
 export enum AppIdentifiers {
   Home,
-  DrilldownLogs,
+
   Explore,
   Dashboard,
+  DrilldownMetrics,
+  DrilldownLogs,
+  DrilldownTraces,
+  DrilldownProfiles,
   Unknown = -1,
 }
 
@@ -23,11 +27,32 @@ export function getAppContext({ pathname }: PageContext): AppContext {
       description: '',
       is_scenes_app: false,
     };
+  } else if (pathname.includes('/explore/metrics')) {
+    return {
+      id: AppIdentifiers.DrilldownMetrics,
+      name: 'Grafana Drilldown Metrics',
+      description: 'The Drilldown Metrics app to browse and analyze metrics',
+      is_scenes_app: false,
+    };
   } else if (pathname.includes('grafana-lokiexplore-app')) {
     return {
       id: AppIdentifiers.DrilldownLogs,
       name: 'Grafana Drilldown Logs',
       description: 'The queryless app to browse Loki Logs',
+      is_scenes_app: false,
+    };
+  } else if (pathname.includes('grafana-pyroscope-app')) {
+    return {
+      id: AppIdentifiers.DrilldownProfiles,
+      name: 'Drilldown Profiles (previously Pyroscope)',
+      description: 'The Drilldown Profiles app to browse and analyze profiles',
+      is_scenes_app: false,
+    };
+  } else if (pathname.includes('grafana-exploretraces-app')) {
+    return {
+      id: AppIdentifiers.DrilldownTraces,
+      name: 'Grafana Drilldown Traces',
+      description: 'The Drilldown Traces app to browse and analyze traces',
       is_scenes_app: false,
     };
   } else if (pathname.includes('/explore')) {
