21621d65ccb89f5cac56aba398e2e0ca69735e47
Matias Chomicki <matyax@gmail.com>
2025-04-09 12:08:58 +0200

    OTel: match log to otel display definitions

--- public/app/features/logs/otel.ts
+++ public/app/features/logs/otel.ts
@@ -1,4 +1,6 @@
-import { LogRowModel } from "@grafana/data";
+import { LogRowModel } from '@grafana/data';
+
+import { LOG_LINE_BODY_FIELD_NAME } from './components/LogDetailsBody';
 
 export interface OTelLogsDisplaySettings {
   name: string;
@@ -8,10 +10,12 @@ export interface OTelLogsDisplaySettings {
 
 interface OTelLogsLabelMatcher {
   labels: Record<string, string>;
+  matcher?: never;
 }
 
 interface OTelLogsCustomMatcher {
   matcher(log: LogRowModel): boolean;
+  labels?: never;
 }
 
 interface OTelLogsDisplayFormat {
@@ -25,7 +29,7 @@ export const definitions: OTelLogsDisplaySettings[] = [
     matcher: {
       labels: {
         severity_number: '',
-      }
+      },
     },
     displayFormat: [
       { label: 'severity_text' },
@@ -33,7 +37,41 @@ export const definitions: OTelLogsDisplaySettings[] = [
       { label: 'scope_name' },
       { label: 'exception_type' },
       { label: 'exception_message' },
-      { label: '__line__' },
-    ]
-  }
+      { label: LOG_LINE_BODY_FIELD_NAME },
+    ],
+  },
 ];
+
+export function getOtelDisplayMessage(log: LogRowModel): string {
+  const definition = matchOtelDefinition(log);
+  if (!definition) {
+    return log.raw;
+  }
+  let line = '';
+  for (const format of definition.displayFormat) {
+    if (format.label === LOG_LINE_BODY_FIELD_NAME) {
+      line += ` ${log.raw}`;
+    } else {
+      line += ` ${log.labels[format.label]}`;
+    }
+  }
+  return line;
+}
+
+export function matchOtelDefinition(log: LogRowModel): OTelLogsDisplaySettings | undefined {
+  for (const definition of definitions) {
+    if (definition.matcher.labels && labelsMatchLog(log, definition.matcher)) {
+      return definition;
+    }
+  }
+  return undefined;
+}
+
+export function labelsMatchLog(log: LogRowModel, matcher: OTelLogsLabelMatcher) {
+  for (const label in matcher.labels) {
+    if (!log.labels[label] || (matcher.labels[label] !== '' && log.labels[label] !== matcher.labels[label])) {
+      return false;
+    }
+  }
+  return true;
+}
